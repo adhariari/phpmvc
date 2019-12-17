0cb0f01d03b526a7a9b939ca28ef31ff0d715907
adhariari <adhari.ariari@gmail.com>
2019-12-17 22:08:42 +0700

    insert data

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,29 +2,42 @@
 
 class Mahasiswa_model  
 {
-    private $dbh;
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-        
-        try
-        {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $error) 
-        {
-            die($error->getMessage());
-        }
+        $this->db = new Database;
     }
-
 
     public function getAllM()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM MAHASISWA');
-        $this->stmt->execute();
-        $hasil = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $hasil;
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
     }
+
+    public function getMahasiswaById($id)   
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+    $this->db->bind('id',$id);
+    return $this->db->singleSet();
+    
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "INSERT INTO mahasiswa
+                    VALUES
+                    ('', :nama, :npm, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('npm', $data['npm']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
 }
--- app/view/mahasiswa/index.php
+++ app/view/mahasiswa/index.php
@@ -1,17 +1,64 @@
-<div class="container mt-5">
+<div class="container mt-3">
 
     <div class="row">
         <div class="col-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
             <h3>Daftar Mahasiswa</h3>
-            <?php foreach ($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?= $mhs['nama']; ?></li>
-                    <li><?= $mhs['npm']; ?></li>
-                    <li><?= $mhs['email']; ?></li>
-                    <li><?= $mhs['jurusan']; ?></li>
-                </ul>
-            <?php endforeach; ?>
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $mhs['nama']; ?>
+                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="badge badge-primary">details</a></li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div>
+</div>
 
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="form-group">
+                        <label for="npm">NPM</label>
+                        <input type="number" class="form-control" id="npm" name="npm">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Sistem Informasi">Sistem Informasi</option>
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Akuntansi">Akuntansi</option>
+                            <option value="Manajemen">Manajemen</option>
+                            <option value="Sastra Inggris">Sastra Inggris</option>
+                        </select>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+
+            </form>
+        </div>
+    </div>
 </div>
